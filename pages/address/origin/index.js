4e2ffdc51d0e0a8594774e0f52cf8d7aa77d28f4
Fluke <jesadakorn.kirtnu@gmail.com>
2021-01-31 03:15:56 +0700

    show address list

--- pages/address/origin/index.js
+++ pages/address/origin/index.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Router from 'next/router'
+import { firestore } from '../../../utils/firebase'
 
 const Address = () => {
+    const [originData, setOriginData] = useState([])
+
     const createRoute = () => {
         Router.push('origin/create')
     }
+
+    const getInitialData = async () => {
+        const originAddress = await firestore.collection('sourceAddress').get()
+        const setData = originAddress.docs.map(item => item.data())
+        setOriginData(setData)
+    }
+
+    useEffect(() => {
+        getInitialData()
+    }, [])
+
     return (
         <div className="address-container">
             <div className="profile-header">
@@ -13,18 +27,32 @@ const Address = () => {
             </div>
             <div className="address-table-wraper">
                 <table>
-                    <tr>
-                        <th>#</th>
-                        <th>ชื่อผู้ส่ง</th>
-                        <th>ที่อยู่</th>
-                        <th>รหัสไปรษณีย์</th>
-                    </tr>
-                    <tr>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>ชื่อผู้ส่ง</th>
+                            <th>ที่อยู่</th>
+                            <th>รหัสไปรษณีย์</th>
+                        </tr>
+                    </thead>
+                    {/* <tr>
                         <td><input type="checkbox" /></td>
                         <td>Address</td>
                         <td>Address</td>
                         <td>Address</td>
-                    </tr>
+                    </tr> */}
+                    <tbody>
+                        {
+                            originData && originData.map((item, key) =>
+                                <tr key={key}>
+                                    <td><input type="checkbox" /></td>
+                                    <td>{item.name}</td>
+                                    <td>{item.address}</td>
+                                    <td>{item.tel}</td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
                 </table>
             </div>
         </div>
