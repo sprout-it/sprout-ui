383ed091a056260cb68e05f7b5cdeada740bde8b
Fluke <jesadakorn.kirtnu@gmail.com>
2021-02-18 01:28:05 +0700

    express custom server and proxy server

--- index.js
+++ index.js
@@ -2,32 +2,38 @@ const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const serviceAccount = require('./config/serviceAccount.json')
 const next = require("next");
-// const bodyParser = require('body-parser')
-const isDev = process.env.development == 'true' || false
-console.log(process.env.development)
+const bodyParser = require('body-parser')
+const isDev = process.env.DEVELOPMENT == 'true' || false
+const axios = require('axios')
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 })
 
-// const dev = process.env.NODE_ENV !== "production";
 const app = next({
-    dev: isDev,
+    // dev: false,
     conf: { distDir: 'nextjs' }
 });
 
 const express = require('express');
 const cors = require('cors');
 const expressServer = express()
-
 const handle = app.getRequestHandler();
-expressServer.use(cors({ origin: true }))
 
-// expressServer.use(cors());
-// expressServer.use(bodyParser.json());
+// expressServer.use(cors({ origin: true }))
+expressServer.use(bodyParser.json());
 
-expressServer.all('*', (req, res) => {
-    return handle(req, res)
+expressServer.all('/proxy/*', async (req, res) => {
+    const [endPointUrl] = Object.values(req.params)
+    axios({
+        method: req.method,
+        url: endPointUrl,
+        data: req.body.length > 0 ? req.body : undefinded
+    });
+})
+
+expressServer.all('*', async (req, res) => {
+    return await handle(req, res)
 });
 
 // expressServer.listen(80, () => console.log('listen at 80'))
@@ -35,7 +41,6 @@ expressServer.all('*', (req, res) => {
 // const server = functions.https.onRequest((req, res) => {
 //     return app.prepare().then(() => handle(req, res))
 // })
-isDev && expressServer.listen(3000, ()=>console.log(`Running on port: 3000`))
+isDev && expressServer.listen(3000, () => console.log(`Running on port: 3000`))
 const server = functions.https.onRequest(expressServer)
-
 exports.nextjs = { server };
